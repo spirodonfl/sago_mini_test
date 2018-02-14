f545f51cd907c680b6952e3cba17274d4a93d19b
Spiro Floropoulos <spirodonfl@gmail.com>
2018-02-14 13:23:51 -0800

    The /api/bump API call is now set to properly handle query with mongo

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ The server manages both the API and also the public HTML served to manipulate da
 
 "npm run-script start_server" etc...
 
+## Running MongoDB
+
+Run MongoDB using the recommended solution for your particular OS. The DB data can live under the "data" folder of this project (or wherever you like).
+
+For example, I run this: '"C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath "X:\sago_mini_test\data"'
+
 ## Building from SCSS to CSS
 
 /public/scss/styles.scss is the scss file that gets compiled into /public/css/styles.css when you run "npm run-script build_css" using node-sass to do it
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var url             = require('url');
 var mongoClient     = require('mongodb').MongoClient;
 var mongoConnection = 'mongodb://localhost:27017';
 var mongoDbName     = 'sago_mini_test';
+var mongoDb         = null;
+var mongoConnected  = null;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,25 +25,42 @@ var defaultData = [
 var router = express.Router();
 router.use(function(req, res, next) {
     console.log('Activity log...');
-    next();
+    // Could have done some sanitization here but I don't think it's 100% necessary
+    mongoClient.connect(mongoConnection, function (err, client) {
+        if (err) {
+            res.statusCode = 500;
+            res.json({ message: 'Serious error' });
+        } else {
+            console.log('Successfully connected to MongoDB');
+            mongoDb = client.db(mongoDbName).collection('bundle_data');
+            mongoConnected = client;
+            next();
+        }
+    });
 });
 router.get('/', function(req, res) {
     res.json({ message: 'Sago Mini API' });
+    mongoConnected.close();
 });
 router.route('/read').get(function (req, res) {
     var parts = url.parse(req.url, true);
     var query = parts.query;
     var bundleId = query.bundle_id.toLowerCase();
-    for (var i = 0; i < defaultData.length; ++i) {
-        var bundleData = defaultData[i];
-        if (bundleData.bundle_id.toLowerCase() === bundleId) {
-            res.statusCode = 200;
-            res.json({ message: 'Found bundle!', data: bundleData });
-            break;
+    mongoDb.find({ bundle_id: bundleId }).toArray(function (err, docs) {
+        mongoConnected.close();
+        if (err) {
+            res.statusCode = 403;
+            res.json({ message: 'Bundle not found' });
+        } else {
+            if (docs.length === 1) {
+                res.statusCode = 200;
+                res.json({ message: 'Found bundle!', bundle: docs[0] });
+            } else {
+                res.statusCode = 403;
+                res.json({ message: 'Bundle not found' });
+            }
         }
-    }
-    res.statusCode = 403;
-    res.json({ message: 'Bundle not found' });
+    });
 });
 router.route('/set').post(function (req, res) {
     var bundleId = req.body.bundle_id.toLowerCase();
@@ -76,28 +95,25 @@ router.route('/set').post(function (req, res) {
 });
 router.route('/bump').post(function (req, res) {
     var bundleId = req.body.bundle_id.toLowerCase();
-    var exists = false;
-    for (var i = 0; i < defaultData.length; ++i) {
-        if (defaultData[i].bundle_id.toLowerCase() === bundleId) {
-            var bundleData = defaultData[i];
-            exists = true;
-            ++bundleData.build_number;
-            res.statusCode = 200;
-            res.json({ message: 'Bundle updated', data: bundleData });
-            break;
-        }
-    }
-    if (!exists) {
-        defaultData.push({
-            id: ddI,
-            bundle_id: bundleId,
-            build_number: 0
+    mongoDb.findOneAndUpdate(
+        { bundle_id: bundleId },
+        { $inc: { build_number: +1 }},
+        { returnOriginal: false, upsert: true },
+        function (err, queryResponse) {
+            mongoConnected.close();
+            if (err) {
+                res.statusCode = 500;
+                res.json({ message: 'Serious error [48]' });
+            } else {
+                console.log(queryResponse);
+                res.statusCode = 200;
+                if (queryResponse.lastErrorObject.updatedExisting) {
+                    res.json({ message: 'Bundle updated', data: queryResponse.value });
+                } else {
+                    res.json({ message: 'Bundle created', data: queryResponse.value });
+                }
+            }
         });
-        var data = defaultData[ddI];
-        ++ddI;
-        res.statusCode = 200;
-        res.json({ message: 'Bundle created', data: data });
-    }
 });
 app.use('/api', router);
 
