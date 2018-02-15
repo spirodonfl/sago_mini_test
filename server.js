faac21bb5cac0a7a5788713e9239a019317f4ff1
Spiro Floropoulos <spirodonfl@gmail.com>
2018-02-14 21:33:48 -0800

    Just a minor mongo class to demonstrate a basic class and some event emitters so you can separate concerns.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app             = express();
 var bodyParser      = require('body-parser');
 var path            = require('path');
 var url             = require('url');
+var eventEmitter    = require('events');
 
 /**
  * Setup of constants (and initializations)
@@ -15,6 +16,41 @@ var mongoConnection = 'mongodb://localhost:27017';
 var mongoDbName     = 'sago_mini_test';
 var mongoDb         = null;
 var mongoConnected  = null;
+/**
+ * Simple Mongo class just so things are a bit more organized
+ */
+var mongoClass = {
+    emitter: new eventEmitter(),
+    mc: require('mongodb').MongoClient,
+    connectionUrl: 'mongodb://localhost:27017', // Could put into a config json file and load it up
+    dbName: 'sago_mini_test', //  Could put into a config json file and load it up
+    db: null,
+    connection: null,
+    connect: function () {
+        this.mc.connect(this.connectionUrl, function (err, client) {
+            if (err) {
+                mongoClass.emitter.emit('Connection error');
+            } else {
+                mongoClass.db = client.db(mongoDbName).collection('bundle_data');
+                mongoClass.connection = client;
+                mongoClass.emitter.emit('Connected');
+            }
+        });
+    },
+    findBundle: function (bundleId) {
+        this.db.find({ bundle_id: bundleId }).toArray(function (err, docs) {
+            if (err) {
+                mongoClass.emitter.emit('Query Error', err);
+            } else {
+                if (docs.length === 1) {
+                    mongoClass.emitter.emit('Found Bundle', docs[0]);
+                } else {
+                    mongoClass.emitter.emit('Bad Query Results');
+                }
+            }
+        });
+    }
+}
 
 /**
  * Tell express that we like to see JSON when we're getting POST (or similar) requests that ship a body
@@ -30,31 +66,17 @@ var port = process.env.PORT || 9000;
 var router = express.Router();
 /**
  * Middleware! Could be used for sanitization.
- * Mongo connection happens (although it could be moved out globally and always on).
- * I opted for a quick instantiation and closure so we don't leave a connection hanging for long periods of time.
- * I realize this might be un-necessary.
  */
 router.use(function(req, res, next) {
     console.log('Activity log...');
     // Could have done some sanitization here but I don't think it's 100% necessary
-    mongoClient.connect(mongoConnection, function (err, client) {
-        if (err) {
-            res.statusCode = 500;
-            res.json({ message: 'Serious error' });
-        } else {
-            console.log('Successfully connected to MongoDB');
-            mongoDb = client.db(mongoDbName).collection('bundle_data');
-            mongoConnected = client;
-            next();
-        }
-    });
+    next();
 });
 /**
  * Default route with no path for API
  */
 router.get('/', function(req, res) {
     res.json({ message: 'Sago Mini API' });
-    mongoConnected.close();
 });
 /**
  * Should look for a bundle id and ship the data back to the client.
@@ -69,23 +91,22 @@ router.route('/read').get(function (req, res) {
         res.statusCode = 400;
         res.statusCode = res.json({ message: 'You need a bundle id' });
     } else {
-        // We lowercase the bundleid here because it might be annoying if someone is typing com.sagomini.Homeworkchallenge and it doesn't matter
-        var bundleId = query.bundle_id.toLowerCase();
-        mongoDb.find({ bundle_id: bundleId }).toArray(function (err, docs) {
-            mongoConnected.close();
-            if (err) {
-                res.statusCode = 403;
-                res.json({ message: 'Bundle not found' });
-            } else {
-                if (docs.length === 1) {
-                    res.statusCode = 200;
-                    res.json({ message: 'Found bundle!', bundle: docs[0] });
-                } else {
-                    res.statusCode = 403;
-                    res.json({ message: 'Bundle not found' });
-                }
-            }
+        mongoClass.emitter.once('Query Error', function (error) {
+            console.log('Query error', error);
+            // I didn't think it was appropriate to show the client (ship to the client) the actual query errors
+            res.statusCode = 403;
+            res.json({ message: 'Bundle not found' });
         });
+        mongoClass.emitter.once('Bad Query Results', function () {
+            res.statusCode = 403;
+            res.json({ message: 'Bundle not found' });
+        });
+        mongoClass.emitter.once('Found Bundle', function (bundle) {
+            res.statusCode = 200;
+            res.json({ message: 'Found bundle!', bundle: bundle });
+        });
+        // We lowercase the bundleid here because it might be annoying if someone is typing com.sagomini.Homeworkchallenge and it doesn't matter
+        mongoClass.findBundle(query.bundle_id.toLowerCase());
     }
 });
 /**
@@ -102,9 +123,8 @@ router.route('/set').post(function (req, res) {
     } else {
         var bundleId = req.body.bundle_id.toLowerCase();
         var newBuildNumber = parseInt(req.body.new_build_number);
-        mongoDb.find({ bundle_id: bundleId }).toArray(function (err, docs) {
+        mongoClass.db.find({ bundle_id: bundleId }).toArray(function (err, docs) {
             if (err) {
-                mongoConnected.close();
                 res.statusCode = 500;
                 // Just a stupid way of id-ing errors in case you need to trace back here easily
                 res.json({ message: 'Serious error [90]' });
@@ -112,12 +132,11 @@ router.route('/set').post(function (req, res) {
                 if (docs.length === 1) {
                     var bundleData = docs[0];
                     if (newBuildNumber > bundleData.build_number) {
-                        mongoDb.findOneAndUpdate(
+                        mongoClass.db.findOneAndUpdate(
                             { bundle_id: bundleId },
                             { $set: { build_number: newBuildNumber }},
                             { returnOriginal: false, upsert: true },
                             function (err, queryResponse) {
-                                mongoConnected.close();
                                 if (err) {
                                     res.statusCode = 500;
                                     res.json({ message: 'Serious error [37]' });
@@ -131,12 +150,10 @@ router.route('/set').post(function (req, res) {
                                 }
                             });
                     } else {
-                        mongoConnected.close();
                         res.statusCode = 400;
                         res.json({ message: 'New build number must be greater than existing (' + bundleData.build_number + ')', bundle: bundleData });
                     }
                 } else {
-                    mongoConnected.close();
                     res.statusCode = 500;
                     res.json({ message: 'Serious error [91]' });
                 }
@@ -146,7 +163,7 @@ router.route('/set').post(function (req, res) {
 });
 /**
  * Simply increments the build number by 1 for a given bundle.
- * Also creates the bundle and sets it if it doesn't exist.
+ * Also creates the bundle and sets it if it doesn't.
  */
 router.route('/bump').post(function (req, res) {
     if (!req.body.bundle_id) {
@@ -154,12 +171,11 @@ router.route('/bump').post(function (req, res) {
         res.statusCode = res.json({ message: 'You need a bundle id' });
     } else {
         var bundleId = req.body.bundle_id.toLowerCase();
-        mongoDb.findOneAndUpdate(
+        mongoClass.db.findOneAndUpdate(
             { bundle_id: bundleId },
             { $inc: { build_number: +1 }},
             { returnOriginal: false, upsert: true },
             function (err, queryResponse) {
-                mongoConnected.close();
                 if (err) {
                     res.statusCode = 500;
                     res.json({ message: 'Serious error [72]' });
@@ -178,5 +194,14 @@ app.use('/api', router);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+/**
+ * First connect to Mongo. Listen to emitters and then carry on.
+ */
+mongoClass.emitter.once('Connected', function () {
+    app.listen(port);
+    console.log('Magic happens on port ' + port);
+});
+mongoClass.emitter.once('Connection Error', function () {
+    throw 'Need MongoDB Connection!';
+});
+mongoClass.connect();
